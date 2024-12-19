ec0edecaa04c65df9e0539662dbf3a03a6aaddc8
bhargavyagnik <bhargavyagnik99@gmail.com>
2024-12-19 11:57:41 -0500

    updated contribution api params

--- src/controllers/contributionController.ts
+++ src/controllers/contributionController.ts
@@ -42,7 +42,7 @@ export const addContribution = async (req: Request, res: Response) => {
 
 export const getContributions = async (req: Request, res: Response) => {
   try {
-    const { eventId } = req.body;
+    const { eventId } = req.params;
     if (!eventId) {
       return res.status(400).json({ message: 'Event ID is required' });
     }
