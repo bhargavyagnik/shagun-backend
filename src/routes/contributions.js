b80eb86cae12f8ea5d6ee2c0535e35e0620cf461
Meet Mehta <meetmehta1198@gmail.com>
2024-12-21 01:27:17 -0500

    Fix js extension files

--- src/routes/contributions.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { addContribution, getContributions, getTotalContributions } from '../controllers/contributionController';
-import { authenticateSession } from '../middleware/auth';
-const router = express.Router();
-router.post('/add', addContribution);
-router.get('/get/:eventId', authenticateSession, getContributions);
-router.get('/gettotal', authenticateSession, getTotalContributions);
-export default router;
